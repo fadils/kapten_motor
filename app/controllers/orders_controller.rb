49a06643cb2dc32bd5669159681ba80e4818a0ab
Fadil Sutomo <fsutomo@gmail.com>
2015-02-09 15:07:48 +0700

    Finish basic order flow

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -81,21 +81,22 @@ class OrdersController < ApplicationController
       @order.attributes = params[:order].permit(:first_name, :last_name, :company, :billing_address1, :billing_address2, :billing_address3, :billing_address4, :billing_country_id, :billing_postcode, :email_address, :phone_number, :delivery_name, :delivery_address1, :delivery_address2, :delivery_address3, :delivery_address4, :delivery_postcode, :delivery_country_id, :separate_delivery_address)
       @order.ip_address = request.ip
       if @order.proceed_to_confirm
-        redirect_to checkout_payment_path
+        #redirect_to checkout_payment_path
+        redirect_to checkout_confirmation_path
       end
     else
       # Add some example order data for the example. In a real application
       # this shouldn't be present.
-      Faker::Config.locale = 'en-gb'
-      @order.first_name = Faker::Name.first_name                                            if @order.first_name.blank?
-      @order.last_name = Faker::Name.last_name                                              if @order.last_name.blank?
-      @order.company = Faker::Company.name                                                  if @order.company.blank?
-      @order.email_address = Faker::Internet.email                                          if @order.email_address.blank?
-      @order.phone_number = Faker::PhoneNumber.phone_number                                 if @order.phone_number.blank?
-      @order.billing_address1 = Faker::Address.building_number + " " + Faker::Address.street_name   if @order.billing_address1.blank?
-      @order.billing_address3 = Faker::Address.city                                                 if @order.billing_address3.blank?
-      @order.billing_address4 = Faker::Address.county                                               if @order.billing_address4.blank?
-      @order.billing_postcode = Faker::Address.zip                                                  if @order.billing_postcode.blank?
+      #Faker::Config.locale = 'en-GB'
+      #@order.first_name = Faker::Name.first_name                                            if @order.first_name.blank?
+      #@order.last_name = Faker::Name.last_name                                              if @order.last_name.blank?
+      #@order.company = Faker::Company.name                                                  if @order.company.blank?
+      #@order.email_address = Faker::Internet.email                                          if @order.email_address.blank?
+      #@order.phone_number = Faker::PhoneNumber.phone_number                                 if @order.phone_number.blank?
+      #@order.billing_address1 = Faker::Address.building_number + " " + Faker::Address.street_name   if @order.billing_address1.blank?
+      #@order.billing_address3 = Faker::Address.city                                                 if @order.billing_address3.blank?
+      #@order.billing_address4 = Faker::Address.country                                               if @order.billing_address4.blank?
+      #@order.billing_postcode = Faker::Address.zip                                                  if @order.billing_postcode.blank?
     end
   end
   
@@ -107,6 +108,8 @@ class OrdersController < ApplicationController
   end
   
   def confirmation
+    @order = Shoppe::Order.find(current_order.id)
+
     unless current_order.confirming?
       redirect_to checkout_path
       return
@@ -119,10 +122,10 @@ class OrdersController < ApplicationController
         # we are adding a payment to the order straight away.
         current_order.payments.create(:method => "Credit Card", :amount => current_order.total, :reference => rand(10000) + 10000, :refundable => true)
         session[:order_id] = nil
-        redirect_to root_path, :notice => "Order has been placed!"
-      rescue Shoppe::Errors::PaymentDeclined => e
-        flash[:alert] = "Payment was declined by the bank. #{e.message}"
-        redirect_to checkout_path
+        redirect_to root_path, :notice => "Pengajuan telah terkirim"
+      #rescue Shoppe::Errors::PaymentDeclined => e
+      #  flash[:alert] = "Payment was declined by the bank. #{e.message}"
+      #  redirect_to checkout_path
       rescue Shoppe::Errors::InsufficientStockToFulfil
         flash[:alert] = "We're terribly sorry but while you were checking out we ran out of stock of some of the items in your basket. Your basket has been updated with the maximum we can currently supply. If you wish to continue just use the button below."
         redirect_to checkout_path
